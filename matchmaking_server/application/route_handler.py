140c2bd92f92c7613a2c8ba320268699004a40de
Philip Maus <philip.maus@web.de>
2024-05-28 18:27:30 +0200

    Added Highscore API-Endpoints to matchmaking server and added highscore table to main page
    /highscores returns the top five highscores with id, name, kills, kills_per_minute, seconds_alive and timestamps, /highscores_html returns a table with the current top 5 highscores, post /highscores with name, kills and seconds_alive adds a new highscore entry

--- matchmaking_server/application/route_handler.py
+++ matchmaking_server/application/route_handler.py
@@ -1,13 +1,16 @@
 import os
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse, JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi import HTTPException
 from application.server import Server
+from application.database import Database
 from application.constants import CLIENT_ROOT_DIR
+from application.models import Highscore
 
 # Default Route Setups 
 # TODO: implement routers
 
-def setup_route_handler(server: Server):
+def setup_route_handler(server: Server, database: Database):
  
     @server.app.get("/")
     async def root():
@@ -39,3 +42,37 @@ def setup_route_handler(server: Server):
             </tr>
         ''' for server in servers])
         return HTMLResponse(content=html)
+    
+    @server.app.get('/highscores')
+    async def get_highscores():
+        highscores = database.getHighscores()
+        return JSONResponse(content=jsonable_encoder(highscores))
+    
+    @server.app.get('/highscores_html')
+    async def get_highscores_html():
+        highscores = database.getHighscores()
+        html = ''.join([f'''
+            <tr>
+                <td class="border px-4 py-2">{highscore['name']}</td>
+                <td class="border px-4 py-2">{highscore['kills']}</td>
+                <td class="border px-4 py-2">{highscore['kills_per_minute']}</td>
+                <td class="border px-4 py-2">{highscore['seconds_alive']}</td>
+                <td class="border px-4 py-2">{highscore['timestamp']}</td>
+            </tr>
+        ''' for highscore in highscores])
+        return HTMLResponse(content=html)
+    
+    @server.app.post('/highscores', status_code=201)
+    async def create_highscore(highscore: Highscore):
+        if len(highscore.name) > 255:
+            raise HTTPException(status_code=400, detail='Name should not be longer than 255 characters')
+        if highscore.kills < 0:
+            raise HTTPException(status_code=400, detail='You can not have less than 0 kills')
+        if highscore.seconds_alive <= 0:
+            raise HTTPException(status_code=400, detail='You must be alive more than 0 seconds')
+        
+        okay = database.addHighscore(highscore.name, highscore.kills, highscore.seconds_alive)
+        if not okay:
+            raise HTTPException(status_code=500, detail='Could not save highscore')
+        
+        return True
--- matchmaking_server/main.py
+++ matchmaking_server/main.py
@@ -1,7 +1,9 @@
 from application.server import Server
+from application.database import Database
 from application.route_handler import setup_route_handler
 
 if __name__ == "__main__":
     server = Server()
-    setup_route_handler(server)
+    database = Database()
+    setup_route_handler(server, database)
     server.run()
