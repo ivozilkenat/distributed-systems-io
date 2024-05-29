04f984de4881e1b9dd3e97cf6f51ed7f9981e975
hoischreck <hoischreck@gmx.de>
2024-05-29 15:34:03 +0200

    repair work

---
+++ services/game_server/backend/route_handler.py
@@ -0,0 +1,19 @@
+import os
+from fastapi.responses import FileResponse, HTMLResponse, JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi import HTTPException
+from backend.server import Server
+from backend.constants import FRONTEND_ROOT_DIR
+
+# Default Route Setups 
+# TODO: implement routers
+
+def setup_route_handler(server: Server):
+ 
+    @server.app.get("/")
+    async def root():
+        return FileResponse(os.path.join(FRONTEND_ROOT_DIR, "html", "index.html"))
+    
+    @server.app.get("/modules/socket.io.js")
+    async def socketio_src():
+        return FileResponse(os.path.join(FRONTEND_ROOT_DIR, "node_modules", "socket.io", "client-dist", "socket.io.js"))
