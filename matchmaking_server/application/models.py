0305bd3bf3ec8ec082bd2f3fe114571a149a0681
hoischreck <hoischreck@gmx.de>
2024-05-29 13:45:29 +0200

    major project structure refactor p1

--- matchmaking_server/application/models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class Highscore(BaseModel):
-    name: str
-    kills: int
-    seconds_alive: int
--- matchmaking_server/main.py
+++
@@ -1,9 +0,0 @@
-from application.server import Server
-from application.database import Database
-from application.route_handler import setup_route_handler
-
-if __name__ == "__main__":
-    server = Server()
-    database = Database()
-    setup_route_handler(server, database)
-    server.run()
