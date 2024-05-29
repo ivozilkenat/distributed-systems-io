07ae35651eb43028f3ab4974ac685f77fb4e2476
hoischreck <hoischreck@gmx.de>
2024-05-29 16:33:20 +0200

    fixed matchmaking_server bugs/constants & fixed service startup

--- services/game_server/service_setup_helpers/start.py
+++ services/game_server/service_setup_helpers/start.py
@@ -1,6 +1,6 @@
 from _utilities import *
             
-def start_server(path=os.path.join(os.getcwd())):
+def start_server(path=os.path.join(os.getcwd(), BACKEND_DIR)):
 
     if not os.path.exists(os.path.join(path, 'venv')):
         print("Missing venv")
@@ -9,10 +9,10 @@ def start_server(path=os.path.join(os.getcwd())):
     print("Activating the virtual environment")
     if sys.platform == "win32":
         # Windows requires a different command to activate the virtual environment
-        run_command(f'cd {path} && .\\venv\\Scripts\\activate && python main.py')
+        run_command(f'cd {path} && .\\venv\\Scripts\\activate && python ../main.py')
     else:
         # Linux and macOS use this command
-        run_command(f'cd {path} && . venv/bin/activate && python3 main.py')
+        run_command(f'cd {path} && . venv/bin/activate && python3 ../main.py')
 
 if __name__ == "__main__":
     start_server()
