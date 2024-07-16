1b426f41619ec565fc63604571e01e5c5e89c667
Ivo Zilkenat <ivo.zilkenat@student.hpi.uni-potsdam.de>
2024-07-16 18:05:05 +0200

    minor refactoring of server.py

--- services/matchmaking_server/backend/constants.py
+++ services/matchmaking_server/backend/constants.py
@@ -3,3 +3,7 @@ from PIL import Image
 
 # SERVER
 FRONTEND_ROOT_DIR = os.path.join(os.path.dirname(__file__), "..", "frontend")
+
+# ENVIRONMENT
+HOST = os.getenv("HOST", "0.0.0.0")
+PORT = int(os.getenv("PORT", "3000"))
