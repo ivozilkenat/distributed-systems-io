86b01ab23eee7ba3065d6622161f40ce0ee9885f
RedToxyl <65180349+RedToxyl@users.noreply.github.com>
2024-07-17 11:40:01 +0200

    reflection

--- services/game_server/backend/game/core.py
+++ services/game_server/backend/game/core.py
@@ -61,11 +61,42 @@ class Projectile (Entity):
         self.uuid = self.generate_uuid()
 
     def move(self):
-        self.pos.x += self.speed * math.cos(self.angle)
-        self.pos.y += self.speed * math.sin(self.angle)
+        newpos = Pos(self.pos.x + self.speed * math.cos(self.angle), self.pos.y + self.speed * math.sin(self.angle))
+        
+        i = 0
+        while self.collides_with_bounds(newpos):
+            self.adjust_angle(newpos)
+            newpos = Pos(self.pos.x + self.speed * math.cos(self.angle), self.pos.y + self.speed * math.sin(self.angle))
+            i += 1
+            if i > 10:
+                self.destroy()
+                break
+            
+        self.pos = newpos
+
         self.traveltime += self.speed
         if self.traveltime >= self.maxrange:
             self.destroy()
+
+    def adjust_angle(self, newpos):
+        # calculate angle of incidence if projectile hits a wall
+        if newpos.x < 0 or newpos.x > X_MAX:
+            self.angle = math.pi - self.angle
+        if newpos.y < 0 or newpos.y > Y_MAX:
+            self.angle = -self.angle
+        
+    def collides_with_bounds(self, newpos):
+        return newpos.x < 0 or newpos.x > X_MAX or newpos.y < 0 or newpos.y > Y_MAX
+    
+    def reset_to_in_bounds(self):
+        if self.pos.x < 0:
+            self.pos.x = 0
+        elif self.pos.x > X_MAX:
+            self.pos.x = X_MAX
+        if self.pos.y < 0:
+            self.pos.y = 0
+        elif self.pos.y > Y_MAX:
+            self.pos.y = Y_MAX
 
     def generate_uuid(self):
         integer_part, fractional_part = str(time.time()).split('.')
